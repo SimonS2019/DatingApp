87f3372e1882dd7dc11893dacf1b3addbd1a1107
Simon Shen <shentiexiong@gmail.com>
2021-09-01 22:12:06 +1200

    38

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
+        // [RegularExpression]
+        // [EmailAddress]
+        // [Phone]
         public string Password { get; set; }
     }
 }
