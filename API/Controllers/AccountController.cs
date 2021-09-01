3e4293d557d86c846985484058b6784b0b306d88
Simon Shen <shentiexiong@gmail.com>
2021-09-01 22:02:44 +1200

    Postman is work well , swagger not

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,8 +3,10 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -20,14 +22,16 @@ namespace API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+            if (await UserExist(registerDto.Username)) return BadRequest("Username is taken");
+
             using var hmac = new HMACSHA512();
             var user = new AppUser
             {
 
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key,
             };
 
@@ -35,6 +39,14 @@ namespace API.Controllers
             await _context.SaveChangesAsync();
 
             return user;
+
+        }
+
+        public async Task<bool> UserExist(string username)
+        {
+
+
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
 
         }
 
