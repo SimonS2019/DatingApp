61e835870631ceac2bd5419174c4b1c6d05ccd09
Simon Shen <shentiexiong@gmail.com>
2021-09-21 23:30:23 +1200

    79

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestErrorsComponent implements OnInit {
   baseUrl = 'https://localhost:5001/api/';
-  // validationErrors: string[] = [];
+  validationErrors: string[] = [];
 
   constructor(private http: HttpClient) { }
 
@@ -50,8 +50,8 @@ export class TestErrorsComponent implements OnInit {
     this.http.post(this.baseUrl + 'account/register', {}).subscribe(response => {
       console.log(response);
     }, error => {
-      console.log(error);
-      // this.validationErrors = error;
+      console.log(error);  
+      this.validationErrors = error;
     })
   }
 }
