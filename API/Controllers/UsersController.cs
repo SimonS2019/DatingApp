510bac98de16d9741decbb8fa3cf44995fcf63ef
Simon Shen <shentiexiong@gmail.com>
2021-09-22 18:46:52 +1200

    89

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -11,6 +11,7 @@ namespace API.Controllers
 {
     // [ApiController]
     // [Route("api/[controller]")]
+            [Authorize]
     public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
@@ -20,14 +21,14 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        [AllowAnonymous]
+        // [AllowAnonymous]
         // [Authorize]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
         }
 
-        [Authorize]
+        // [Authorize]
         // [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
