6354a8b9e63ad87ad6c501a36e54679414a3cd36
Simon Shen <shentiexiong@gmail.com>
2021-09-24 01:11:14 +1200

    End of S9

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 
 @NgModule({
   declarations: [],
@@ -12,8 +13,9 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
     }),
-    TabsModule.forRoot()
+    TabsModule.forRoot(),
+    NgxGalleryModule
   ],
-  exports: [BsDropdownModule, ToastrModule,TabsModule],
+  exports: [BsDropdownModule, ToastrModule,TabsModule,NgxGalleryModule],
 })
 export class SharedModule {}
