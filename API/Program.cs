e32a11e7bb80a04d91ded1ce438196eecfad3833
MotivDesktop1 <shentiexiong@gmail.com>
2023-05-04 16:49:59 +1200

    end of S3

--- API/Program.cs
+++ API/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:5200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:5200"));
 
 
 app.MapControllers();
