42e4416cbad0e1e0e252a369a12182fee92c0244
Simon Shen <shentiexiong@gmail.com>
2023-05-22 23:11:24 +1200

    40 4:42

--- API/Program.cs
+++ API/Program.cs
@@ -18,6 +18,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:5200"));
 
+// app.UseAuthentication();
+// app.UseAuthorization();
 
 app.MapControllers();
 
