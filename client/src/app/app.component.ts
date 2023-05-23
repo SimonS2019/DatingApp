cef3350a46112028d9798a9feda4836e3398ab13
MotivDesktop1 <shentiexiong@gmail.com>
2023-05-24 09:41:02 +1200

    git_contribution_test3

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -26,7 +26,7 @@ export class AppComponent implements OnInit {
     this.http.get('https://localhost:5001/api/users').subscribe({
       next: response => this.users = response,
       error: error => console.log(error),
-      complete: () => console.log('Request has completed')
+      complete: () => console.log('Request has completed123')
     })
   }
 
