6de3082ac4409bea183e3af9c7f623c79cc579c5
MotivDesktop1 <shentiexiong@gmail.com>
2023-05-26 16:06:40 +1200

    53

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
-// import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   declarations: [
@@ -20,7 +20,7 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    // BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
