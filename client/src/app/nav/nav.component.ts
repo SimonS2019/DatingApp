1c8267188d3e54e47abcf5978c24fe0ccffb9c08
MotivDesktop1 <shentiexiong@gmail.com>
2023-05-26 14:44:44 +1200

    49

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  model: any = {};
 
+  // constructor(public accountService: AccountService) { }
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  login() {
+    console.log(this.model);
+    
+    // this.accountService.login(this.model).subscribe({
+    //   next: response => {
+    //     console.log(response);
+    //   },
+    //   error: error => console.log(error)
+    // })
+  }
+
+  // logout() {
+  //   this.accountService.logout();
+  // }
 
 }
