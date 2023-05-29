cc95e14646e59bbe6d3a0e2f66c3f65f9cbd29c3
MotivDesktop1 <shentiexiong@gmail.com>
2023-05-29 16:39:26 +1200

    55

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -4,33 +4,42 @@ import { AccountService } from '../_services/account.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
   model: any = {};
   loggedIn = false;
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService) {}
 
   ngOnInit(): void {
+    this.getCurrentUser()
+  }
+
+  getCurrentUser() {
+    
+    this.accountService.currentUser$.subscribe({
+      next: user => (this.loggedIn = !!user),
+      
+      error: error => console.log(error),
+    });
   }
 
   login() {
     console.log(this.model);
-    
+
     this.accountService.login(this.model).subscribe({
-      next: response => {
+      next: (response) => {
         console.log(response);
-        this.loggedIn =true;
+        this.loggedIn = true;
       },
-      error: error => console.log(error)
-    })
+      error: (error) => console.log(error),
+    });
   }
 
   logout() {
-    this.loggedIn =false;
+    this.accountService.logout();
 
-    // this.accountService.logout();
+    this.loggedIn = false;
   }
-
 }
