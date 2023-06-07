d89f9aefec3b2b2c1d38b0b69e816e90c61242f3
MotivDesktop1 <shentiexiong@gmail.com>
2023-06-07 16:10:43 +1200

    76

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,11 @@
 using API.Extensions;
-
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
@@ -14,6 +13,8 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:5200"));
 
 app.UseAuthentication();
