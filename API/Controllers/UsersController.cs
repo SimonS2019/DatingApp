de86cb97d74a2ff3104fa61d54d5dfcf77e594a6
MotivDesktop1 <shentiexiong@gmail.com>
2023-06-13 10:28:08 +1200

    95

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,28 +16,38 @@ namespace API.Controllers
 
     public class UsersController : BaseApiController
     {
+        private readonly IMapper _mapper;
 
         private readonly IUserRepository _userRepository;
-        public UsersController(IUserRepository userRepository)
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
+            _mapper = mapper;
             _userRepository = userRepository;
         }
 
         [HttpGet]
 
 
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        // public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
 
-            return Ok(await _userRepository.GetUsersAsync());
+            // return Ok(await _userRepository.GetUsersAsync());
+
+            var users = await _userRepository.GetUsersAsync();
+            var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
+            return Ok(usersToReturn);
         }
 
 
         [HttpGet("{username}")]
 
-        public async Task<ActionResult<AppUser>> GetUser(string username)
+        // public async Task<ActionResult<AppUser>> GetUser(string username)
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            return Ok(await _userRepository.GetUserByUsernameAsync(username));
+            // return Ok(await _userRepository.GetUserByUsernameAsync(username));
+             var user = await _userRepository.GetUserByUsernameAsync(username);
+            return _mapper.Map<MemberDto>(user);
         }
 
         // [HttpGet("{id}")]
