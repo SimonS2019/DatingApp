de1ad7701129c1127e1893e9ff643698bc095f2e
MotivDesktop1 <shentiexiong@gmail.com>
2023-06-13 16:20:44 +1200

    107

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -12,11 +12,11 @@ export class MembersService {
   constructor(private http: HttpClient) {}
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users', this.getHttpOption());
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + 'users/' + username, this.getHttpOption());
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
 
   }
 
