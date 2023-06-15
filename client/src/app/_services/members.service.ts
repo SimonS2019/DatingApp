ea991391e20f4c234f4ec1ca72b55a7772b386da
MotivDesktop1 <shentiexiong@gmail.com>
2023-06-15 13:14:14 +1200

    121 end of S 10

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
-import { map } from 'rxjs';
+import { map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -14,20 +14,32 @@ export class MembersService {
   constructor(private http: HttpClient) {}
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    if (this.members.length > 0) return of(this.members);
+    //of运算符用于将值包装为 Observable 并立即发射它。当你想要将同步值作为 Observable 返回时，通常会使用它。
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    )
   }
 
   getMember(username: string) {
+    const member = this.members.find(x => x.userName === username);
+    if (member) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
   updateMember(member: Member) {
-    return this.http.put(this.baseUrl + 'users', member)
-    // return this.http.put(this.baseUrl + 'users', member).pipe(
-    //   map(() => {
-    //     const index = this.members.indexOf(member);
-    //     this.members[index] = { ...this.members[index], ...member };
-    //   })
-    // );
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        //返回数组中值第一次出现的索引，如果不存在则返回 -1。
+        this.members[index] = {...this.members[index], ...member}
+        //...member：使用展开运算符
+        //这段代码 this.members[index] = {...this.members[index], ...member} 
+        //用于使用 member 对象中提供的新值更新 members 数组中的特定元素。
+      })
+    )
   }
 }
