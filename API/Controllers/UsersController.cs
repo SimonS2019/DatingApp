1cbe98520d37d2e10fc0dfe2ec5d3e1be4e1c989
Simon Shen <shentiexiong@gmail.com>
2023-06-19 20:49:33 +1200

    129

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -84,9 +84,9 @@ namespace API.Controllers
 
             if (await _userRepository.SaveAllAsync()) 
             {
-                return _mapper.Map<PhotoDto>(photo);
-                // return CreatedAtAction(nameof(GetUser), 
-                //     new {username = user.UserName}, _mapper.Map<PhotoDto>(photo));
+                // return _mapper.Map<PhotoDto>(photo);
+                return CreatedAtAction(nameof(GetUser), 
+                    new {username = user.UserName}, _mapper.Map<PhotoDto>(photo));
             }
 
 
