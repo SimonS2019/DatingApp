1155b5f8d0830dd3ccc1535101a6151ef4a7fafd
SimonS2019 <shentiexiong@gmail.com>
2023-06-21 15:14:48 +1200

    140

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,20 +12,36 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
 
+  registerForm: FormGroup = new FormGroup({});
+
   constructor(private accountService: AccountService,private toastr: ToastrService) { }
 
-  ngOnInit(): void {    
+  ngOnInit(): void { 
+    this.initializeForm();
+   
   }
-
-  register() {    
-    this.accountService.register(this.model).subscribe({
-      next: () => {
-        this.cancel();
-      },
-      error: error =>         {this.toastr.error(error.error),
-      console.log(error);
-      }
-    })
+  initializeForm() {
+    // this.registerForm = this.fb.group({
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl(),
+    });
+    // this.registerForm.controls['password'].valueChanges.subscribe({
+    //   next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    // })
+  }
+  register() { 
+    console.log(this.registerForm?.value);
+       
+    // this.accountService.register(this.model).subscribe({
+    //   next: () => {
+    //     this.cancel();
+    //   },
+    //   error: error =>         {this.toastr.error(error.error),
+    //   console.log(error);
+    //   }
+    // })
   }
 
   cancel() {
