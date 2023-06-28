dc48079800e2c8832c47cca71b9c453bd0df82cd
SimonS2019 <shentiexiong@gmail.com>
2023-06-28 15:39:39 +1200

    155

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 import { map, of } from 'rxjs';
+import { PaginatedResult } from '../_models/pagination';
 
 @Injectable({
   providedIn: 'root',
@@ -10,18 +11,34 @@ import { map, of } from 'rxjs';
 export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
+  paginatedResult: PaginatedResult<Member[]> = new PaginatedResult<Member[]>;
 
   constructor(private http: HttpClient) {}
 
-  getMembers() {
-    if (this.members.length > 0) return of(this.members);
-    //of运算符用于将值包装为 Observable 并立即发射它。当你想要将同步值作为 Observable 返回时，通常会使用它。
-    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
-      map(members => {
-        this.members = members;
-        return members;
+  getMembers(page?:number, itemsPerPage? :number) {
+
+
+    let params = new HttpParams();
+    if (page && itemsPerPage) {
+      params = params.append('pageNumber', page);
+      params = params.append('pageSize', itemsPerPage);
+    }
+
+    return this.http.get<Member[]>(this.baseUrl + 'users',{observe:'response',params}).pipe(
+      map(response=>{
+        if (response.body) {
+          this.paginatedResult.result =response.body;
+        }
+        const pagination =response.headers.get('Pagination');
+        if (pagination) {
+          this.paginatedResult.pagination = JSON.parse(pagination);
+        }
+        return this.paginatedResult;
       })
     )
+
+ 
+    
   }
 
   getMember(username: string) {
