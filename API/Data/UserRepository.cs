7a3326d89a75052cc901910042802aa9c9a9164e
SimonS2019 <shentiexiong@gmail.com>
2023-07-03 11:41:56 +1200

    161

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -52,6 +52,11 @@ namespace API.Data
             var maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MinAge));
             query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
             
+            query = userParams.OrderBy switch
+            {
+                "created" => query.OrderByDescending(u => u.Created),
+                _ => query.OrderByDescending(u => u.LastActive)
+            };
             return await PagedList<MemberDto>.CreateAsync(query.AsNoTracking().ProjectTo<MemberDto>(_mapper.ConfigurationProvider), userParams.PageNumber, userParams.PageSize);
             // var query = _context.Users.AsQueryable();
 
@@ -63,11 +68,6 @@ namespace API.Data
 
             // query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
 
-            // query = userParams.OrderBy switch
-            // {
-            //     "created" => query.OrderByDescending(u => u.Created),
-            //     _ => query.OrderByDescending(u => u.LastActive)
-            // };
 
             // return await PagedList<MemberDto>.CreateAsync(
             //     query.AsNoTracking().ProjectTo<MemberDto>(_mapper.ConfigurationProvider), 
