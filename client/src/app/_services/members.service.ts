49d70d64734e8b7341aecc2ae0dbd751fbe30988
SimonS2019 <shentiexiong@gmail.com>
2023-07-03 14:49:08 +1200

    168 end of S13

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,9 +2,11 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
-import { map, of } from 'rxjs';
+import { map, of, take } from 'rxjs';
 import { PaginatedResult } from '../_models/pagination';
 import { UserParams } from '../_models/userParams';
+import { AccountService } from './account.service';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root',
@@ -13,37 +15,106 @@ export class MembersService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
   memberCache = new Map();
+  user: User | undefined;
+  userParams: UserParams | undefined;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private accountService: AccountService
+  ) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe({
+      next: (user) => {
+        if (user) {
+          this.userParams = new UserParams(user);
+          this.user = user;
+        }
+      },
+    });
+  }
 
   getMembers(userParams: UserParams) {
-console.log(Object.values(userParams).join('-'));
-const response= this.memberCache.get(Object.values(userParams).join('-'));
-if (response) return of (response);
-console.log(this.memberCache);
+    //     console.log(this.memberCache);
 
-    let params = this.getPaginationHeaders(userParams.pageNumber,userParams.pageSize);
+    // console.log(Object.values(userParams).join('-'));
+    const response = this.memberCache.get(Object.values(userParams).join('-'));
+    if (response) return of(response);
+    // console.log(this.memberCache);
+
+    let params = this.getPaginationHeaders(
+      userParams.pageNumber,
+      userParams.pageSize
+    );
     params = params.append('minAge', userParams.minAge);
     params = params.append('maxAge', userParams.maxAge);
     params = params.append('gender', userParams.gender);
     params = params.append('orderBy', userParams.orderBy);
 
-    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users', params)
-    .pipe(
-      map(response => {
+    return this.getPaginatedResult<Member[]>(
+      this.baseUrl + 'users',
+      params
+    ).pipe(
+      map((response) => {
         this.memberCache.set(Object.values(userParams).join('-'), response);
         return response;
       })
-    )
-
- 
-    
+    );
   }
 
+  getUserParams() {
+    return this.userParams;
+  }
+
+  setUserParams(params: UserParams) {
+    this.userParams = params;
+  }
+
+  resetUserParams() {
+    if (this.user) {
+      this.userParams = new UserParams(this.user);
+      return this.userParams;
+    }
+    return;
+  }
+
+
+  getMember(username: string) {
+    // console.log(this.memberCache);
+
+    // const member = this.members.find(x => x.userName === username);
+    // if (member) return of(member);
+    const member = [...this.memberCache.values()]
+      .reduce((arr, elem) => arr.concat(elem.result), [])
+      .find((member: Member) => member.userName === username);
+
+    if (member) return of(member);
+
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        //返回数组中值第一次出现的索引，如果不存在则返回 -1。
+        this.members[index] = { ...this.members[index], ...member };
+        //...member：使用展开运算符
+        //这段代码 this.members[index] = {...this.members[index], ...member}
+        //用于使用 member 对象中提供的新值更新 members 数组中的特定元素。
+      })
+    );
+  }
+
+  setMainPhoto(photoId: number) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
+  }
+
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
+  }
   private getPaginatedResult<T>(url: string, params: HttpParams) {
-    const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>;
+    const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>();
     return this.http.get<T>(url, { observe: 'response', params }).pipe(
-      map(response => {
+      map((response) => {
         if (response.body) {
           paginatedResult.result = response.body;
         }
@@ -56,48 +127,12 @@ console.log(this.memberCache);
     );
   }
 
-  private getPaginationHeaders(pageNumber:number, pageSize:number) {
+  private getPaginationHeaders(pageNumber: number, pageSize: number) {
     let params = new HttpParams();
 
-      params = params.append('pageNumber', pageNumber);
-      params = params.append('pageSize', pageSize);
-  
+    params = params.append('pageNumber', pageNumber);
+    params = params.append('pageSize', pageSize);
+
     return params;
   }
-
-  getMember(username: string) {
-    // console.log(this.memberCache);
-    
-    // const member = this.members.find(x => x.userName === username);
-    // if (member) return of(member);
-    const member = [...this.memberCache.values()]
-    .reduce((arr, elem) => arr.concat(elem.result), [])
-    .find((member: Member) => member.userName === username);
-
-  if (member) return of(member);
-    
-    return this.http.get<Member>(this.baseUrl + 'users/' + username);
-  }
-
-  updateMember(member: Member) {
-    return this.http.put(this.baseUrl + 'users', member).pipe(
-      map(() => {
-        const index = this.members.indexOf(member);
-        //返回数组中值第一次出现的索引，如果不存在则返回 -1。
-        this.members[index] = {...this.members[index], ...member}
-        //...member：使用展开运算符
-        //这段代码 this.members[index] = {...this.members[index], ...member} 
-        //用于使用 member 对象中提供的新值更新 members 数组中的特定元素。
-      })
-    )
-  }
-
-  setMainPhoto(photoId: number) {
-    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
-  }
-
-  deletePhoto(photoId: number) {
-    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
-  }
-
 }
