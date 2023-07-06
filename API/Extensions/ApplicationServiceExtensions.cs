a7a92eac9cae6445b6af80e3446726c6f7362859
SimonS2019 <shentiexiong@gmail.com>
2023-07-06 13:39:00 +1200

    182

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -23,8 +23,10 @@ namespace API.Extensions
             services.AddScoped<IPhotoService, PhotoService>();
             services.AddScoped<LogUserActivity>();
             services.AddScoped<ILikesRepository, LikesRepository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
 
-            
+
+
             return services;
         }
     }
