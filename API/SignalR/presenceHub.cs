9eb187334415638fe04f69605c2f977204fc4d1d
SimonS2019 <shentiexiong@gmail.com>
2023-07-18 09:17:27 +1200

    223

--- API/SignalR/presenceHub.cs
+++ API/SignalR/presenceHub.cs
@@ -4,29 +4,34 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace API.SignalR
 {
-     [Authorize]
+    [Authorize]
     public class PresenceHub : Hub
     {
-        //  private readonly PresenceTracker _tracker;
-        //     public PresenceHub(PresenceTracker tracker)
-        //     {
-        //         _tracker = tracker;
-        //     }
+        private readonly PresenceTracker _tracker;
+        public PresenceHub(PresenceTracker tracker)
+        {
+            _tracker = tracker;
+        }
 
         public override async Task OnConnectedAsync()
         {
+            await _tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
             await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
             // var isOnline = await _tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
             // if (isOnline)
             //     await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
 
-            // var currentUsers = await _tracker.GetOnlineUsers();
-            // await Clients.Caller.SendAsync("GetOnlineUsers", currentUsers);
+            var currentUsers = await _tracker.GetOnlineUsers();
+            await Clients.Caller.SendAsync("GetOnlineUsers", currentUsers);
         }
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
+            await _tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
             await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+            var currentUsers = await _tracker.GetOnlineUsers();
+            await Clients.Caller.SendAsync("GetOnlineUsers", currentUsers);
+
             await base.OnDisconnectedAsync(exception);
 
             // var isOffline = await _tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
